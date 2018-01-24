1c3452cb6be64bd7892468de33870a08aebb06a1
James Steele <jamessteele87@outlook.com>
2018-01-24 12:50:16 +0000

    refactor test_scores table to include kata and student id

--- sql/student-database.sql
+++ sql/student-database.sql
@@ -4,35 +4,41 @@ CREATE DATABASE student_database;
 
 \c student_database;
 
-CREATE TABLE katas (
-  kata_id SERIAL PRIMARY KEY,
-  kata_name VARCHAR
-);
-
-CREATE TABLE test_scores (
-  test_scores_id SERIAL PRIMARY KEY,
-  test_score INT DEFAULT 0
-);
-
-
 CREATE TABLE students (
-  student_id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   username VARCHAR,
   user_password VARCHAR
 );
 
+CREATE TABLE katas (
+  id SERIAL PRIMARY KEY,
+  kata_name VARCHAR
+);
+
+CREATE TABLE test_scores (
+  id SERIAL PRIMARY KEY,
+  test_score INT NOT NULL,
+  kata_id INT NOT NULL,
+  student_id INT NOT NULL,
+  FOREIGN KEY (kata_id) REFERENCES katas(id),
+  FOREIGN KEY (student_id) REFERENCES students(id)
+);
+
+
+
 INSERT INTO katas (kata_name) VALUES
 ('pigLatin'), ('calculateDivisors');
 
-INSERT INTO test_scores (test_score) VALUES
-(0), (20);
-
 INSERT INTO students (username, user_password) VALUES
-('Northcoder', 'password123');
+('Northcoder1', 'password123'), ('Northcoder2', 'password123');
 
-SELECT kata_id, kata_name, test_scores.test_score FROM katas JOIN test_scores ON katas.kata_id = test_scores_id;
+INSERT INTO test_scores (test_score, kata_id, student_id) VALUES
+(0,1,1), (20,2,1), (40,1,2), (60,2,2);
+-- SELECT kata_id, kata_name, test_scores.test_score FROM katas JOIN test_scores ON katas.kata_id = test_scores_id;
+
+-- SELECT student_id, username, katas.kata_name FROM students JOIN test_scores ON 
 
 
 SELECT * FROM katas;
-SELECT * FROM test_scores;
 SELECT * FROM students;
+SELECT * FROM test_scores;
