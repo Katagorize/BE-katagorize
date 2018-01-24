766177ebf9fb64ce3f5b79c578e627f4530d55fb
James Steele <jamessteele87@outlook.com>
2018-01-24 11:40:08 +0000

    create table for katas

--- sql/student-database.sql
+++ sql/student-database.sql
@@ -4,6 +4,11 @@ CREATE DATABASE student_database;
 
 \c student_database;
 
+CREATE TABLE katas (
+  kata_id SERIAL PRIMARY KEY,
+  kata_name VARCHAR
+);
+
 CREATE TABLE test_scores (
   test_scores_id SERIAL PRIMARY KEY,
   test_score INT DEFAULT 0
@@ -16,6 +21,9 @@ CREATE TABLE students (
   user_password VARCHAR
 );
 
+INSERT INTO katas (kata_name) VALUES
+('pigLatin'), ('calculateDivisors');
+
 INSERT INTO test_scores (test_score) VALUES
 (0), (0);
 
@@ -23,6 +31,6 @@ INSERT INTO students (username, user_password) VALUES
 ('Northcoder', 'password123');
 
 
-
+SELECT * FROM katas;
 SELECT * FROM test_scores;
 SELECT * FROM students;
