762cb4fe7070b33be694c1168d964428db5a9dd8
James Steele <jamessteele87@outlook.com>
2018-01-24 11:33:22 +0000

    create sql table fro students and test scores

---
+++ sql/student-database.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS student_database;
+
+CREATE DATABASE student_database;
+
+\c student_database;
+
+CREATE TABLE test_scores (
+  test_scores_id SERIAL PRIMARY KEY,
+  test_score INT DEFAULT 0
+);
+
+
+CREATE TABLE students (
+  student_id SERIAL PRIMARY KEY,
+  username VARCHAR,
+  user_password VARCHAR
+);
+
+INSERT INTO test_scores (test_score) VALUES
+(0), (0);
+
+INSERT INTO students (username, user_password) VALUES
+('Northcoder', 'password123');
+
+
+
+SELECT * FROM test_scores;
+SELECT * FROM students;
