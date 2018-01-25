556787c15abe163e412175b38955e335d95fb986
Christopher Peers <christopher_peers@outlook.com>
2018-01-25 09:56:43 +0000

    added the production and test db. works off proccess.env

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+const PORT = require('./config').PORT;
 const express = require('express');
 const app = express();
 const apiRouter = require('./routes');
 
 app.use('/api', apiRouter);
 
-app.listen(3000, () => {console.log('listening on 3000...')});
+app.listen(PORT, () => {console.log(`listening on ${PORT}...`)});
---
+++ sql/student-database-test.sql
@@ -0,0 +1,54 @@
+DROP DATABASE IF EXISTS student_database;
+
+CREATE DATABASE student_database;
+
+\c student_database;
+
+CREATE EXTENSION pgcrypto;
+
+CREATE TABLE students (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR,
+  user_password VARCHAR,
+  user_image VARCHAR
+);
+
+CREATE TABLE katas (
+  id SERIAL PRIMARY KEY,
+  kata_name VARCHAR
+);
+
+CREATE TABLE test_scores (
+  id SERIAL PRIMARY KEY,
+  test_score INT NOT NULL,
+  kata_id INT NOT NULL,
+  student_id INT NOT NULL,
+  FOREIGN KEY (kata_id) REFERENCES katas(id),
+  FOREIGN KEY (student_id) REFERENCES students(id)
+);
+
+
+
+INSERT INTO katas (kata_name) VALUES
+('pigLatin'), ('calculateDivisors');
+
+INSERT INTO students (username, user_password) VALUES
+('Northcoder1', crypt('password1', gen_salt('bf', 8))), ('Northcoder', crypt('passFSGRDword1', gen_salt('bf', 8)));
+
+INSERT INTO test_scores (test_score, kata_id, student_id) VALUES
+(0,1,1), (20,2,1), (40,1,2), (60,2,2);
+
+-- Get all students with kata name and score
+SELECT students.username, kata_name, test_scores.test_score FROM katas 
+JOIN test_scores ON katas.id = test_scores.kata_id
+JOIN students ON students.id = test_scores.student_id; 
+-- Get an individual's kata's and scores
+SELECT students.username, kata_name, test_scores.test_score FROM katas 
+JOIN test_scores ON katas.id = test_scores.kata_id
+JOIN students ON students.id = test_scores.student_id 
+WHERE students.username = 'Northcoder';
+
+
+SELECT * FROM students;
+SELECT * FROM katas;
+SELECT * FROM test_scores;
