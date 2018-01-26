9d8d6789cde059658524a6c957e9d77ddefb9e07
Christopher Peers <christopher_peers@outlook.com>
2018-01-26 12:58:06 +0000

    commit needed to switch to dev to update database with more test data

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,5 +1,30 @@
-function getAllUsers () {
-    console.log('getting all users');
+const pgp = require('pg-promise')({promiseLib: Promise})
+const config = require('../../config').DB
+const db = pgp(config)
+
+function getAllUsers (req, res) {
+
+    db.many(`SELECT students.username, kata_name, test_scores.test_score, test_scores.test_date FROM katas 
+        JOIN test_scores ON katas.id = test_scores.kata_id
+        JOIN students ON students.id = test_scores.student_id;`)
+        .then(allUsers => {
+
+            return allUsers.reduce((acc, user, i) => {
+                console.log(i, acc)
+                let test = acc.filter(el => el.name !== acc.name) 
+                console.log('test', i)
+                
+                acc.push({
+                    name: user.username,
+                    katas: [{kataName: user.kata_name,
+                    scores: [user.test_score]}]
+                })
+                console.log(acc)
+                return acc 
+            
+            }, [])
+        })
+        .then(data => res.send(data))
 }
 
 function getSingleUser () {
