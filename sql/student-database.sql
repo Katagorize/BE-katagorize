a3de8522831293e2287b21dc2517a7589e549791
James Steele <jamessteele87@outlook.com>
2018-01-26 09:05:21 +0000

    update notes on sql data base

--- sql/student-database.sql
+++ sql/student-database.sql
@@ -38,17 +38,39 @@ INSERT INTO students (username, user_password) VALUES
 INSERT INTO test_scores (test_score, kata_id, student_id) VALUES
 (0,1,1), (20,2,1), (40,1,2), (60,2,2);
 
+
+
 -- Get all students with kata name and score
-SELECT students.username, kata_name, test_scores.test_score FROM katas 
-JOIN test_scores ON katas.id = test_scores.kata_id
-JOIN students ON students.id = test_scores.student_id; 
+-- SELECT students.username, kata_name, test_scores.test_score FROM katas 
+-- JOIN test_scores ON katas.id = test_scores.kata_id
+-- JOIN students ON students.id = test_scores.student_id;
+
+
 -- Get an individual's kata's and scores
-SELECT students.username, kata_name, test_scores.test_score FROM katas 
-JOIN test_scores ON katas.id = test_scores.kata_id
-JOIN students ON students.id = test_scores.student_id 
-WHERE students.username = 'Northcoder';
+-- SELECT students.username, kata_name, test_scores.test_score FROM katas 
+-- JOIN test_scores ON katas.id = test_scores.kata_id
+-- JOIN students ON students.id = test_scores.student_id 
+-- WHERE students.username = 'Northcoder'; 
+
+
+-- update an individual kata score
+-- UPDATE test_scores SET test_score = 100 FROM (SELECT students.username, kata_name, test_scores.test_score FROM katas 
+-- JOIN test_scores ON katas.id = test_scores.kata_id
+-- JOIN students ON students.id = test_scores.student_id 
+-- WHERE students.id = 1) AS test_score WHERE id = 1;
+
+
+-- authenticate the password for a user
+-- SELECT * FROM students WHERE 
+--     username='Northcoder' AND 
+--     user_password = crypt('passFSGRDword1', user_password);
 
 
 SELECT * FROM students;
 SELECT * FROM katas;
 SELECT * FROM test_scores;
+
+-- table when incorrect password input
+-- SELECT * FROM students WHERE 
+--     username='Northcoder' AND 
+--     user_password = crypt('padfsssFSGRDword1', user_password);
