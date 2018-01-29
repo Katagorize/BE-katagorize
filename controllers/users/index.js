41e25eb3b45e8583905c463286a4cd7869fe1674
Jade <jadeelizabethryan@outlook.com>
2018-01-29 11:06:20 +0000

    Post database (#16)
    * post request adds users to the database
    
    * linting
    
    * userLogin verifies password
    
    * check user does not exist in the db

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,43 +1,64 @@
-const pgp = require('pg-promise')({ promiseLib: Promise })
-const config = require('../../config').DB
-const db = pgp(config)
+const pgp = require('pg-promise')({ promiseLib: Promise });
+const config = require('../../config').DB;
+const db = pgp(config);
 
 function getAllUsers(req, res) {
 
-    db.many(`SELECT students.username, kata_name, test_scores.test_score, test_scores.test_date FROM katas 
+  db.many(`SELECT students.username, kata_name, test_scores.test_score, test_scores.test_date FROM katas 
         JOIN test_scores ON katas.id = test_scores.kata_id
         JOIN students ON students.id = test_scores.student_id;`)
-        .then(allUsers => {
-            let result = {};
-            allUsers.map((el, i) => {
+    .then(allUsers => {
+      let result = {};
+      allUsers.map((el) => {
 
-                if (Object.keys(result).includes(el.username)) {
-                    if(Object.keys(result[el.username]).includes(el.kata_name)) {
-                        result[el.username][el.kata_name].push(`${el.test_date} score: ${el.test_score}`);
-                    }
-                    else {
-                        result[el.username][el.kata_name] = [`${el.test_date} score: ${el.test_score}`];
-                    }
+        if (Object.keys(result).includes(el.username)) {
+          if (Object.keys(result[el.username]).includes(el.kata_name)) {
+            result[el.username][el.kata_name].push(`${el.test_date} score: ${el.test_score}`);
+          }
+          else {
+            result[el.username][el.kata_name] = [`${el.test_date} score: ${el.test_score}`];
+          }
 
-                }
-                else {
-                    let date = el.test_date;
-                    result[el.username] = {}
-                    result[el.username][el.kata_name] = [`${el.test_date} score: ${el.test_score}`];
-                }
-            })
-            return result;
-        })
-        .then(data => res.send(data))
+        }
+        else {
+          let date = el.test_date;
+          result[el.username] = {};
+          result[el.username][el.kata_name] = [`${date} score: ${el.test_score}`];
+        }
+      });
+      return result;
+    })
+    .then(data => res.send(data));
 }
 
-function getSingleUser (req, res) {
-    console.log(req.params, '***************');
-    db.one('SELECT username, user_image FROM students WHERE username = $1;', req.params.user_name)
+function getSingleUser(req, res) {
+  db.one('SELECT username, user_image FROM students WHERE username = $1;', req.params.user_name)
     .then((data) => {
-        res.send(data);
+      res.send(data);
+    });
+}
+
+function addUser(req, res) {
+  db.any('SELECT * FROM students WHERE username = $1;', [req.params.user_name])
+    .then((student) => {
+      if (student.length > 0) res.send('user already exists')
+      else {
+        db.one('INSERT INTO students (username, user_password, user_image) VALUES ($1, crypt($2, gen_salt(\'bf\', 8)), $3) RETURNING *;', [req.params.user_name, req.body.password, req.body.user_image])
+          .then((data) => {
+            res.send(data);
+          });
+      }
     })
-};
+    .catch(error => console.log(error))
 
+}
 
-module.exports = { getAllUsers, getSingleUser };
+function userLogin(req, res) {
+  db.one('SELECT * FROM students WHERE username=$1 AND user_password = crypt($2, user_password)', [req.params.user_name, req.headers.authorization])
+    .then((data) => {
+      res.send(data);
+    })
+    .catch(error => console.log(error));
+}
+
+module.exports = { getAllUsers, getSingleUser, addUser, userLogin };
--- spec/getSingleScore.spec.js
+++ spec/getSingleScore.spec.js
@@ -1,30 +1,29 @@
-process.env.NODE_ENV = 'development'
-const {expect} = require('chai')
-const app = require('../app')
-const getSingleScore = require('../controllers/scores/index')
-const request = require('supertest')(app)
+process.env.NODE_ENV = 'development';
+const {expect} = require('chai');
+const app = require('../app');
+const request = require('supertest')(app);
 
 
 describe('getSingleScore', () => {
-    it('returns a json file with a complete test report', () => {
-        return request
-        .get('/api/users/katagorize-student/katas/pigLatin/test')
-        .expect(200)
-        .then(res => {
-            expect(res.body).to.be.an('object')
-            expect(res.body.tests).to.be.an('array')
-            expect(res.body.failures).to.be.an('array')
-            expect(res.body.passes).to.be.an('array')
-            return;
-        })
-    });
-    it('returns a 404 error if kata does not exist on students repo', () => {
-        return request
-        .get('/api/users/katagorize-student/katas/newKata/test')
-        .expect(404)
-        .then(res => {
-            expect(res.body.message).to.equal('Kata not found')
-            return;
-        })
-    })
-})
+  it('returns a json file with a complete test report', () => {
+    return request
+      .get('/api/users/katagorize-student/katas/pigLatin/test')
+      .expect(200)
+      .then(res => {
+        expect(res.body).to.be.an('object');
+        expect(res.body.tests).to.be.an('array');
+        expect(res.body.failures).to.be.an('array');
+        expect(res.body.passes).to.be.an('array');
+        return;
+      });
+  });
+  it('returns a 404 error if kata does not exist on students repo', () => {
+    return request
+      .get('/api/users/katagorize-student/katas/newKata/test')
+      .expect(404)
+      .then(res => {
+        expect(res.body.message).to.equal('Kata not found');
+        return;
+      });
+  });
+});
