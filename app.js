b60a415b5c116c868171eea4029a62042135cb3a
Semra Dora <semra.refkaewa@gmail.com>
2018-01-30 17:47:04 +0000

    remove morgan forever

--- app.js
+++ app.js
@@ -4,13 +4,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 const PORT = require('./config').PORT;
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
 const apiRouter = require('./routes');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 app.use(cors());
-app.use(morgan('combined'));
 app.use(bodyParser.json());
 
 app.use('/api', apiRouter);
